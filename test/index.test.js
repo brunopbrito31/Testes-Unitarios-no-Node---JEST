ccfbc8864b881695a75bd21af7aa6f05f71f8e95
brunopbrito31 <brunopbrito31@hotmail.com>
2022-07-13 23:37:52 -0300

    Ajuste de nomenclaturas

--- test/index.test.js
+++ test/index.test.js
@@ -3,18 +3,18 @@ const homeIndex = require('../index');
 const funcaoSoma = homeIndex.soma;
 const funcaoMultiplicacao = homeIndex.multiplicacao;
 
-test('Deve retornar a soma de 2 + 2', () => {
+test('soma de 2 + 2 deve retornar 4', () => {
     expect(funcaoSoma(2,2)).toBe(4);
 });
 
-test('Função de soma deve retornar resultado do tipo numérico', () => {
+test('soma deve retornar resultado do tipo numérico', () => {
     expect(typeof funcaoSoma(2,2)).toBe('number');
 });
 
-test('Função deve retornar a multiplicação de 2 x 2', () => {
+test('multiplicacao de 2 x 2 deve retornar 6', () => {
     expect(funcaoMultiplicacao(2,3)).toBe(6);
 });
 
-test('Função de multiplicação deve retornar resultado do tipo numérico', () => {
+test('multiplicação deve retornar resultado do tipo numérico', () => {
     expect(typeof funcaoMultiplicacao(2,3)).toBe('number');
 });
